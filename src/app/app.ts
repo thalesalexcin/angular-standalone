77584f0d420bd289139c22223d4215f122446021
thalesalexcin <thalesalexcin@gmail.com>
2025-11-26 17:06:10 +0100

    Updating app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    templateUrl: './app.html',
+    styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('angular-standalone');
+export class AppComponent {
+    title = 'angular-standalone';
 }
