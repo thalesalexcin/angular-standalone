9b471d9957ba7a3380cc6b945ee74745a2c80f94
thalesalexcin <thalesalexcin@gmail.com>
2025-11-28 12:02:26 +0100

    ng model

--- src/app/components/formulaire-simple/formulaire-simple.ts
+++ src/app/components/formulaire-simple/formulaire-simple.ts
@@ -1,22 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-simple',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulaire-simple.html',
   styleUrl: './formulaire-simple.css',
 })
 export class FormulaireSimpleComponent {
   nom = 'Doe';
-  nomSignal = signal('');
-  onInputChanged(e: Event) {
-    if (e.target instanceof HTMLInputElement) {
-      let value = (e.target as HTMLInputElement).value;
-      this.nomSignal.set(value);
-    }
-  }
-
-  updateNom(value: string) {
-    this.nom = value;
-  }
 }
