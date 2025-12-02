afe98fbf91fb97fda86cfce0f34ec13a911bdfd9
thalesalexcin <thalesalexcin@gmail.com>
2025-12-02 09:09:34 +0100

    @ViewChild

--- src/app/components/pere/pere.ts
+++ src/app/components/pere/pere.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FilsComponent } from '../fils/fils';
 
 @Component({
@@ -8,6 +8,12 @@ import { FilsComponent } from '../fils/fils';
   styleUrl: './pere.css',
 })
 export class PereComponent {
+  @ViewChild(FilsComponent) fils!: FilsComponent;
   nom = 'John Wick';
   ville = 'Marseille';
+
+  premierEnfant() {
+    console.log(this.fils);
+    console.log(this.fils.ville);
+  }
 }
