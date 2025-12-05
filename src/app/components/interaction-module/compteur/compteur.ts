6b1c9751867cbbfdb360c82d919d87ded5d5a867
thalesalexcin <thalesalexcin@gmail.com>
2025-12-05 11:53:15 +0100

    using addBy

--- src/app/components/interaction-module/compteur/compteur.ts
+++ src/app/components/interaction-module/compteur/compteur.ts
@@ -1,18 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { decrement, increment } from '../../../stores/counter/counter.action';
+import { addBy, decrement, increment } from '../../../stores/counter/counter.action';
 import { selectValue } from '../../../stores/counter/counter.selector';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-compteur',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './compteur.html',
   styleUrl: './compteur.css',
 })
 export class CompteurComponent {
   value: number = 0;
+  valueToAdd: number = 0;
   store = inject(Store);
   //value$: Observable<Number>;
   constructor() {
@@ -25,5 +27,9 @@ export class CompteurComponent {
   }
   onDecrement() {
     this.store.dispatch(decrement());
+  }
+
+  onAddBy() {
+    this.store.dispatch(addBy({ value: this.valueToAdd }));
   }
 }
